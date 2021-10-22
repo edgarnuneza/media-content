8fbc8583f421aafc6e7756377be10e1c89cf279b
Edgar <edgar.nunez.9998@gmail.com>
2021-10-21 20:40:11 -0600

    Mayoria es responsive

--- src/app/main-navbar/main-navbar.component.ts
+++ src/app/main-navbar/main-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-main-navbar',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-navbar.component.scss']
 })
 export class MainNavbarComponent implements OnInit {
+  @ViewChild('menubtn', { static: false }) btnMenu!: ElementRef;
+  @ViewChild('options', { static: false }) options!: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.renderer.listen(this.btnMenu.nativeElement, "click", () => {
+      this.options.nativeElement.classList.toggle("show");
+
+    });
+
   }
 
 }
