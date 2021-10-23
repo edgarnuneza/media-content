521df85457e7a2de8109ce3dcc5c3bda318956ba
Edgar <edgar.nunez.9998@gmail.com>
2021-10-23 12:45:19 -0600

    Final

--- src/app/media-content/media-content.component.ts
+++ src/app/media-content/media-content.component.ts
@@ -38,8 +38,8 @@ export class MediaContentComponent implements OnInit {
 
   getImagePath(img: any) {
     for (let i of img) {
-      console.log(`https://thewebgun.com${i.pathThumbnail}`);
-      this.images.push(`https://thewebgun.com${i.pathThumbnail}`);
+      console.log(`../assets/${i.pathThumbnail}`);
+      this.images.push(`../assets/${i.pathThumbnail}`);
     }
   }
 
